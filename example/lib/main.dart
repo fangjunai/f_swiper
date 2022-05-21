2a48e7c31b282044b25da1b512cd353ed26b4929
fangjun <13671936161@139.com>
2022-05-21 10:58:24 +0800

    publish v0.0.1

---
+++ example/lib/main.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'dart:async';
+
+import 'package:flutter/services.dart';
+import 'package:f_swiper/f_swiper.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  //Picture URL, onTap
+  List<Map<String, Function>> list = [
+    {
+      "https://s.alicdn.com/@img/imgextra/i2/O1CN01aBX2LK1urj81SOFzR_!!6000000006091-2-tps-968-230.png":
+          () => {}
+    },
+    {
+      "https://s.alicdn.com/@img/imgextra/i3/O1CN012JlDJ41PV3UMIeqjA_!!6000000001845-2-tps-968-230.png":
+          () => print("onTap => Picture2")
+    },
+    {
+      "https://s.alicdn.com/@img/imgextra/i4/O1CN01lQNPo226RsAsyZvwz_!!6000000007659-2-tps-968-230.png":
+          () => print("onTap => Picture3")
+    }
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('FSwiper Example App'),
+        ),
+        body: Container(
+          height: 100,
+          child: FSwiper(list: list),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/f_swiper.dart
@@ -0,0 +1,66 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+
+class FSwiper extends StatefulWidget {
+  final List<Map<String, Function>> list;
+
+  const FSwiper({Key? key, required this.list}) : super(key: key);
+
+  @override
+  _FSwiperState createState() => _FSwiperState();
+}
+
+class _FSwiperState extends State<FSwiper> with SingleTickerProviderStateMixin {
+  int _pageIndex = 0;
+  TabController? _tabController;
+
+  @override
+  void initState() {
+    _tabController = TabController(length: widget.list.length, vsync: this);
+    Timer.periodic(const Duration(seconds: 3), (timer) {
+      if (_pageIndex < widget.list.length) {
+        _tabController?.animateTo(_pageIndex);
+        _pageIndex++;
+      } else {
+        _tabController?.animateTo(0);
+        _pageIndex = 1;
+      }
+    });
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Container(
+            alignment: Alignment.center,
+            width: MediaQuery.of(context).size.width,
+            child: Stack(alignment: Alignment.bottomCenter, children: [
+              TabBarView(controller: _tabController, children: items),
+              TabPageSelector(
+                controller: _tabController,
+                indicatorSize: 6,
+                color: Colors.white,
+              )
+            ])));
+  }
+
+  List<Widget> get items {
+    List<Widget> list = [];
+    for (var map in widget.list) {
+      map.forEach((key, callback) {
+        list.add(Container(
+            alignment: Alignment.center,
+            child: GestureDetector(
+                child: Image.network(
+                  key,
+                  fit: BoxFit.fill,
+                  width: MediaQuery.of(context).size.width,
+                ),
+                onTap: () => callback())));
+      });
+    }
+    return list;
+  }
+}
